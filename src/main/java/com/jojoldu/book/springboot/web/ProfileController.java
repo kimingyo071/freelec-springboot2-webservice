a6f85529c53b04a401743428cca5f45b7f943762
ingyo <kimingyo071@gmail.com>
2020-09-17 14:29:10 +0900

    395 진행 // 오타 수정

--- src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
+++ src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
@@ -19,6 +20,9 @@ public class ProfileController {
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
-        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
